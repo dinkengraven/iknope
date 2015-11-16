b2033994b0d4782fdddca2cb19d25280708cabb7
dinkengraven <katelyn.dinkgrave@gmail.com>
2015-11-15 22:31:23 -0600

    initial commit

--- iknope/ViewController.swift
+++ iknope/ViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
+let adjectives = ["beautiful", "poetic", "noble", "talented", "brilliant", "strong", "powerful", "cunning", "pliable", "chestnut-haired", "wonderful", "tropical", "naive", "sophisticated", "rule-breaking", "smart", "new-born", "tricky", "devious", "coy", "glowing", "majestic"]
+
+let nouns = ["musk ox", "sunfish", "baby", "moth", "spinster", "bastard", "sun-goddess", "leopard seal"]
+
+extension Array {
+    func sample() -> Element {
+        let randomIndex = Int(arc4random_uniform(UInt32(self.count)))
+        return self[randomIndex]
+    }
+}
+
+func makeKnopism() {
+    "Oh Ann, you \(adjectives.sample()), \(adjectives.sample()), \(adjectives.sample()) \(nouns.sample())."
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var message: UILabel!
+    
+    
+    @IBAction func knopeButton(sender: UIButton) {
+        message.text = "Oh Ann, you \(adjectives.sample()), \(adjectives.sample()), \(adjectives.sample()) \(nouns.sample())."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +41,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
+
+
+
 
